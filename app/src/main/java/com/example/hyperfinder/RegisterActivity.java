eed6ddc2f6b0f018e2de96a92b8ae8472816126d
jabilers <rancheta122@gmail.com>
2022-10-25 23:10:50 +0800

    update register activity

--- app/src/main/java/com/example/hyperfinder/RegisterActivity.java
+++ app/src/main/java/com/example/hyperfinder/RegisterActivity.java
@@ -19,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText email, password;
+    private EditText fname, mname, lname, age, sex, contact;
     private Button btnRegister;
     private TextView textLogin;
     @Override
@@ -27,6 +28,12 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
+        fname = findViewById(R.id.register_firstName);
+        mname = findViewById(R.id.register_mName);
+        lname = findViewById(R.id.register_lastName);
+        age = findViewById(R.id.register_age);
+        sex = findViewById(R.id.register_sex);
+        contact = findViewById(R.id.register_contactNumber);
         email = findViewById(R.id.register_email);
         password = findViewById(R.id.register_password);
         btnRegister = findViewById(R.id.register);
@@ -48,12 +55,30 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void Register() {
+        String firstName = fname.getText().toString().trim();
+        String midName = mname.getText().toString().trim();
+        String lastname = lname.getText().toString().trim();
+        String yr = age.getText().toString().trim();
+        String gender = sex.getText().toString().trim();
+        String cNumber = contact.getText().toString().trim();
         String user = email.getText().toString().trim();
         String pass = password.getText().toString().trim();
         if (user.isEmpty()) {
             email.setError("Email can not be empty");
         } if (pass.isEmpty()) {
             password.setError("Password can not be empty");
+        } if (firstName.isEmpty()) {
+            fname.setError("First Name can not be empty");
+        } if (midName.isEmpty()) {
+            mname.setError("Middle Name can not be empty");
+        } if (lastname.isEmpty()) {
+            lname.setError("Last Name can not be empty");
+        } if (yr.isEmpty()) {
+            age.setError("Age can not be empty");
+        } if (gender.isEmpty()) {
+            sex.setError("Sex can not be empty");
+        } if (cNumber.isEmpty()) {
+            contact.setError("Contact Number can not be empty");
         } else {
             mAuth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
